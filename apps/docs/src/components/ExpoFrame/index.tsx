9e66bcdf389e0fc5617215d57e16a5f2956ccdc8
Serge Harb <me@sergeharb.com>
2023-11-02 12:49:25 +0100

    add loading state

--- apps/docs/src/components/ExpoFrame/index.tsx
+++ apps/docs/src/components/ExpoFrame/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 const BASE_URL = process.env.EXPO_PREVIEW_URL;
 
 type Props = {
@@ -7,12 +9,41 @@ type Props = {
 };
 
 export const ExpoFrame = (props: Props) => {
+  const [loading, setLoading] = useState(true);
+
+  const handleLoad = () => {
+    setLoading(false);
+  };
+
+  if (!BASE_URL) {
+    return null;
+  }
+
   return (
-    <iframe
-      src={`${BASE_URL}${props.url}`}
-      style={{ borderRadius: 8 }}
-      height={props.height}
-      width={props.width}
-    />
+    <div style={{ position: 'relative', borderRadius: 8, backgroundColor: '#282a36' }}>
+      {loading && (
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <div>Loading...</div>
+        </div>
+      )}
+      <iframe
+        allowTransparency
+        src={`${BASE_URL}${props.url}`}
+        height={props.height}
+        width={props.width}
+        onLoad={handleLoad}
+      />
+    </div>
   );
 };
