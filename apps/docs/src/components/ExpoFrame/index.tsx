d9d607a7d56cd96729e327c241b731c5c8da7466
Serge Harb <me@sergeharb.com>
2023-11-02 15:58:28 +0100

    simplify iframe

--- apps/docs/src/components/ExpoFrame/index.tsx
+++ apps/docs/src/components/ExpoFrame/index.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-
 const BASE_URL = process.env.EXPO_PREVIEW_URL;
 
 type Props = {
@@ -9,40 +7,17 @@ type Props = {
 };
 
 export const ExpoFrame = (props: Props) => {
-  const [loading, setLoading] = useState(true);
-
-  const handleLoad = () => {
-    setLoading(false);
-  };
-
   if (!BASE_URL) {
     return null;
   }
 
   return (
-    <div style={{ position: 'relative', borderRadius: 8, backgroundColor: '#282a36' }}>
-      {loading && (
-        <div
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <div>Loading...</div>
-        </div>
-      )}
+    <div style={{ borderRadius: 8, backgroundColor: '#282a36' }}>
       <iframe
         allowTransparency
         src={`${BASE_URL}${props.url}`}
         height={props.height}
         width={props.width}
-        onLoad={handleLoad}
       />
     </div>
   );
