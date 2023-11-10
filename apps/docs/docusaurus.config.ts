e661883a4e1b609744ef611bf0c966d75d7e10e2
Serge Harb <me@sergeharb.com>
2023-11-10 13:24:35 +0100

    improve docs for v1

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -37,9 +37,8 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl: 'https://github.com/nodegram/nativeflare',
+          sidebarCollapsible: false,
+          sidebarCollapsed: false,
         },
         blog: {
           showReadingTime: true,
